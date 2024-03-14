0948f8bfbf924a824c1246d1fe675efbc2e6e04d
Mizu <psdk55052004@gmail.com>
2024-03-14 15:04:18 +0800

    匯入 vconsole

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import 'bootstrap'
 
 import App from './App.vue'
 
+import VConsole from 'vconsole'
+
 const pinia = createPinia()
 const app = createApp(App)
+const vConsole = new VConsole({ theme: 'dark' })
+console.log(vConsole)
 
 app.use(router)
 app.use(pinia)
