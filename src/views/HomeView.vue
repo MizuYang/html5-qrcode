13c4227a91731d9b6297029b7791bc8fd4fc5a8d
Mizu <psdk55052004@gmail.com>
2024-03-14 15:04:56 +0800

    修改 html5qrcode

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,90 +1,77 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-// 可以自定義開啟相機的class
-import { Html5Qrcode } from 'html5-qrcode'
-
-// qrCode實體
-const html5QrCode = ref(null)
-const searchProductName = ref('')
-
-onMounted(() => {
-  console.log('Html5Qrcode', Html5Qrcode)
-
-  html5QrCode.value = new Html5Qrcode('barcode-reader')
-
-  console.log('html5QrCode', html5QrCode)
-})
-
-// 初始化要在 onMounted 中 （要在virtual dom render之後才抓得到 dom）
-onMounted(() => {
-})
-
-// 掃描成功後會得到 decodedText
-const qrCodeSuccessCallback = (decodedText) => {
-  // 傳入 searchProductName 去做後續處理
-  searchProductName.value = decodedText
-  // 停止相機
-  handleStop()
-}
-
-// 啟動相機  開啟掃描功能
-// 傳入 qrCodeSuccessCallback 掃描成功後要做的事
-const start = (qrCodeSuccessCallback) => {
-  // 相機brcode顯示設定
-  const brConfig = { fps: 10, qrbox: { width: 300, height: 150 } }
-
-  // 套件啟動相機function
-  html5QrCode.value.start(
-    // 使用預設前或後鏡頭 （environment 為使用預設）
-    { facingMode: 'environment' },
-    // 相機brcode顯示設定
-    brConfig,
-    // 掃描成功後的 Callback
-    qrCodeSuccessCallback
-    // Error的 Callback
-    // qrCodeErrorCallback
-  )
-}
-// // 關閉相機
-const handleStop = () => {
-  try {
-    // 套件關閉相機function
-    html5QrCode.value
-      .stop()
-      .then(() => {
-        // 清除
-        html5QrCode.value.clear()
-      })
-      .catch((error) => {
-        console.log('danger', error.message)
-      })
-  } catch (error) {
-    console.log('danger', error)
-  }
-}
-// 啟動相機
-const scanBarcode = () => {
-  start(qrCodeSuccessCallback)
-  // html5QrCode.value.start(qrCodeSuccessCallback)
-}
-// 停止相機
-// const handleStop = () => {
-//   html5QrCode.value.handleStop()
-// }
-
-</script>
-
 <template>
-  <button type='button'
-          class='btn btn-primary'
-          @click="scanBarcode">
-    開啟相機權限
-  </button>
-
-  <!-- barcode 相機初始化 -->
-  <div id="barcode-reader" class="component-barcode-reader"></div>
-
-  "{{ searchProductName }}"
+  <div class="home">
+    这是首页
+  </div>
+  <hr/>
+  <button @click="getCameras">扫码</button>  ====  <button @click="stop">取消扫码</button>
+  <div style="height: 100%; width: 100%">
+    <!-- <MyHeader :name="'调用摄像头扫码'" left="arrow-left" @goBackEv="$emit('goBack')" /> -->
+    <div class="qrcode">
+      <div id="reader"></div>
+    </div>
+  </div>
 </template>
 
-<style lang='scss' scope></style>
+<script setup>
+import { Html5Qrcode } from 'html5-qrcode'
+
+let html5QrCode = null
+
+const start = () => {
+  html5QrCode.start(
+    // environment后置摄像头 user前置摄像头
+    { facingMode: 'environment' },
+    {
+      fps: 2, // 可选，每秒帧扫描二维码
+      qrbox: { width: 250, height: 250 } // 可选，如果你想要有界框UI
+      // aspectRatio: 1.777778 // 可选，视频馈送需要的纵横比，(4:3--1.333334, 16:9--1.777778, 1:1--1.0)传递错误的纵横比会导致视频不显示
+    },
+    (decodedText, decodedResult) => {
+      // do something when code is read
+      console.log('decodedText', decodedText)
+      console.log('decodedResult', decodedResult)
+      // this.$emit("goBack", decodedText)
+    }
+  )
+    .catch((err) => {
+      console.log('扫码错误信息', err)
+      // 错误信息处理仅供参考，具体情况看输出！！！
+      if (typeof err === 'string') {
+        // this.$toast(err)
+      } else {
+        // if (err.name == 'NotAllowedError') return this.$toast("您需要授予相机访问权限")
+        // if (err.name == 'NotFoundError') return this.$toast('这个设备上没有摄像头')
+        // if (err.name == 'NotSupportedError') return this.$toast('摄像头访问只支持在安全的上下文中，如https或localhost')
+        // if (err.name == 'NotReadableError') return this.$toast('相机被占用')
+        // if (err.name == 'OverconstrainedError') return this.$toast('安装摄像头不合适')
+        // if (err.name == 'StreamApiNotSupportedError') return this.$toast('此浏览器不支持流API')
+      }
+    })
+}
+
+const getCameras = () => {
+  Html5Qrcode.getCameras()
+    .then((devices) => {
+      if (devices && devices.length) {
+        html5QrCode = new Html5Qrcode('reader')
+        start()
+      }
+    })
+    .catch(() => {
+      // handle err
+      html5QrCode = new Html5Qrcode('reader')
+      // this.$toast('您需要授予相机访问权限')
+    })
+}
+
+const stop = () => {
+  html5QrCode.stop().then((ignore) => {
+    // QR Code scanning is stopped.
+    console.log('QR Code scanning stopped.')
+  })
+    .catch(() => {
+      // Stop failed, handle it.
+      console.log('Unable to stop scanning.')
+    })
+}
+</script>
